08f960ee8f35aac8ff347fb9cbeb160f04848d98
JashanSingh66 <jashan.js111@gmail.com>
2026-03-11 13:45:50 +0530

    Saving multiple links

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+
+// for toggling of the form 
+
+const add=document.getElementById("addBtn");
+const formPanel=document.getElementsByClassName("panel");
+add.addEventListener("click",function(e){
+    e.preventDefault();
+    formPanel[0].classList.toggle("hidden");
+})
+
+
+
 const data=[];
 const form=document.getElementById("addLinkPanel");
 form.addEventListener("submit",function(event){
@@ -12,29 +24,34 @@ form.addEventListener("submit",function(event){
     }
     data.push(link);
     print(link);
+    formPanel[0].classList.toggle("hidden");
     console.log(data);
+    
 })
 function print(link){
     console.log(link);
     const linkData=document.getElementById("linkData");
-    const newdiv=document.createElement("div");
-    const content=document.createTextNode("Data");
-    newdiv.appendChild(content);
-    linkData.appendChild(newdiv);
+    const p1=document.createElement("p");
+    const p2=document.createElement("p");     
+    const c1=document.createTextNode(link.name);
+    const c2=document.createTextNode(link.catg);
+    p1.appendChild(c1);
+    p2.appendChild(c2);
+    const newDiv=document.createElement("div");
+
+    newDiv.appendChild(p1);
+    newDiv.appendChild(p2);
+    newDiv.addEventListener("click",function(e){
+        e.preventDefault();
+        console.log(link.url);
+    })
+    linkData.appendChild(newDiv);
     
 
 } 
 console.log(form.name);
 
 
-// for toggling of the form 
-
-const add=document.getElementById("addBtn");
-const formPanel=document.getElementsByClassName("panel");
-add.addEventListener("click",function(e){
-    e.preventDefault();
-    formPanel[0].classList.toggle("hidden");
-})
 
 
 
